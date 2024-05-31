d26daa809a67684f1cf8619eb66e3c240d6ed44a
D4rrylPrt <112367562+Seoren-343@users.noreply.github.com>
2024-05-31 17:32:29 +0700

    commit lastt anjjggg

--- my-app/resources/views/busFol/buscreation.blade.php
+++ my-app/resources/views/busFol/buscreation.blade.php
@@ -22,7 +22,6 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Your Website</a>
         <ul class="navbar-nav">
             <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Home</a></li>
             <li class="nav-item"><a class="nav-link" href="{{ url('founders') }}">Founder</a></li>
